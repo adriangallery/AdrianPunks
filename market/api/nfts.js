1b804eabb7f8697b3079504fe115edfa8be29b06
ADRIAN <adriangallery@adriancerda.com>
2025-04-03 19:33:00 +0200

    Corregido error de visualización de imágenes y reemplazado SQL.js por API local

--- market/api/nfts.js
+++ market/api/nfts.js
@@ -1,6 +1,62 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
+const nfts = [
+    {
+        token_id: "1",
+        image_url: "https://ipfs.io/ipfs/bafybeibfywb3emvjod5owcus7nyn4fqosqrbvuq2cyxczhbmavfxuautsy/1.gif",
+        attributes: '{"Background": "Blue", "Type": "Human", "Accessory": "None"}'
+    },
+    {
+        token_id: "2",
+        image_url: "https://ipfs.io/ipfs/bafybeibfywb3emvjod5owcus7nyn4fqosqrbvuq2cyxczhbmavfxuautsy/2.gif",
+        attributes: '{"Background": "Red", "Type": "Alien", "Accessory": "Hat"}'
+    },
+    // Agrega más NFTs aquí según sea necesario
+];
+
+// Función para obtener todos los NFTs
+function getAllNFTs() {
+    return nfts;
+}
+
+// Función para obtener NFTs filtrados
+function getFilteredNFTs(filters) {
+    return nfts.filter(nft => {
+        const attributes = JSON.parse(nft.attributes);
+        return (!filters.background || attributes.Background === filters.background) &&
+               (!filters.type || attributes.Type === filters.type) &&
+               (!filters.accessory || attributes.Accessory === filters.accessory);
+    });
+}
+
+// Función para obtener atributos únicos
+function getUniqueAttributes() {
+    const backgrounds = new Set();
+    const types = new Set();
+    const accessories = new Set();
+
+    nfts.forEach(nft => {
+        const attributes = JSON.parse(nft.attributes);
+        if (attributes.Background) backgrounds.add(attributes.Background);
+        if (attributes.Type) types.add(attributes.Type);
+        if (attributes.Accessory) accessories.add(attributes.Accessory);
+    });
+
+    return {
+        backgrounds: Array.from(backgrounds),
+        types: Array.from(types),
+        accessories: Array.from(accessories)
+    };
+}
+
+// Exportar las funciones
+window.nftAPI = {
+    getAllNFTs,
+    getFilteredNFTs,
+    getUniqueAttributes
+};
+
 module.exports = (req, res) => {
   // La ruta al archivo .db. Se asume que está en la raíz del proyecto.
   const dbPath = path.join(__dirname, '..', 'nft_metadata.db');
