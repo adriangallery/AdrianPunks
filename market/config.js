f874452d8431bd909503f688bdd5453cb6c4f8f9
ADRIAN <adriangallery@adriancerda.com>
2025-04-03 20:10:23 +0200

    Actualización completa del marketplace para funcionar online

--- market/config.js
+++ market/config.js
@@ -1,6 +1,6 @@
 window.appConfig = {
-    apiBaseUrl: 'api',
-    componentsBaseUrl: 'components',
+    apiBaseUrl: "https://adrianpunks.com/market/api",
+    componentsBaseUrl: "https://adrianpunks.com/market/components",
     publicBaseUrl: 'public',
     ethersVersion: '5.7.2'
 }; 
